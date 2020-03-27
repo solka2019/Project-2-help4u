36aac2d735f311df8f8f790d93abdde07d73225c
Marissol <marfkar@gmail.com>
2020-03-26 19:49:45 -0700

    add timeout to help with heroku

--- server.js
+++ server.js
@@ -26,6 +26,13 @@ var routes = require("./controllers/controller");
 app.use(routes);
 app.use(express.static('public/assets/img')); 
 
+// Timeout to ensure everything is wired-up when running in Heroku
+// this is ok to be here also for local runs... it will only slowdown a bit!
+function haltOnTimedout(req, res, next) {
+  if (!req.timedout) next();
+}
+
+app.use(haltOnTimedout);
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
